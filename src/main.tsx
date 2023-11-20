6f8a3cf211f0ceb5cf78ab31d73db913d19416d1
Yonghun <yonghuni484@gmail.com>
2023-11-20 17:34:34 +0900

    Env: develop 브랜치 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import App from "./App.tsx";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <BrowserRouter>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
