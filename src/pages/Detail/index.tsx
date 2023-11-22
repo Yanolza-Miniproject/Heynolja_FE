cb9627fd736277f4e5847a624ab39b0ec6180fbb
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-22 11:53:03 +0900

    Chore: react-slick 설치

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,5 +1,55 @@
+import React from "react";
+import * as Styled from "./Detail.styles.ts";
+import ProductGallery from "../../components/Detail/ProductGallery";
+import ProductDetails from "../../components/Detail/ProductDetails";
+import QuantitySelector from "../../components/Detail/QuantitySelector";
+import PriceDisplay from "../../components/Detail/PriceDisplay";
+import ActionButtonGroup from "../../components/Detail/ActionButtonGroup";
+import Calendar from "../../components/Detail/Calendar/index.tsx";
+import StockStatusBanner from "../../components/Detail/StockStatusBanner/index.tsx";
+// 상품 상세 정보와 이미지를 가정한 데이터입니다.
+const productData = {
+  // images: [
+  //   "https://github.com/Yanolza-Miniproject/frontend/assets/92326949/fd904255-0d68-46df-a091-18d6efc6427f",
+  // ],
+  images: [
+    "https://github.com/Yanolza-Miniproject/frontend/assets/92326949/4d1ffd05-195e-4bc3-aafe-67800ccfe83c",
+    "https://github.com/Yanolza-Miniproject/frontend/assets/92326949/fd904255-0d68-46df-a091-18d6efc6427f",
+  ],
+  name: "그랜드 하얏트 제주",
+  roomName: "디럭스룸",
+  price: "239,000원",
+  quantity: 1,
+};
+
 const Detail = () => {
-  return <h1>상품 상세 페이지</h1>;
+  return (
+    <Styled.container>
+      <Styled.Layout>
+        <ProductGallery images={productData.images} />
+        <Styled.DetailsContainer>
+          <Styled.HorizontalContainer>
+            <ProductDetails
+              roomName={productData.roomName}
+              name={productData.name}
+              price={productData.price}
+            />
+            <StockStatusBanner />
+          </Styled.HorizontalContainer>
+          <Calendar price={productData.price} />
+          <QuantitySelector
+            initialQuantity={productData.quantity}
+            onQuantityChange={(newQuantity) => console.log(newQuantity)}
+            price={productData.price}
+          />
+          <PriceDisplay price={productData.price} />
+          <ActionButtonGroup
+            onAddToCart={() => console.log("Add to Cart clicked")}
+          />
+        </Styled.DetailsContainer>
+      </Styled.Layout>
+    </Styled.container>
+  );
 };
 
 export default Detail;
