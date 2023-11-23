be0cd87d1f8ef1cedbb241a8d7a79d186b40588d
Yonghun <yonghuni484@gmail.com>
2023-11-23 23:31:34 +0900

    Feat: 결제 완료 상품에 대한 리코일 파일 추가

--- src/store/purchaseAtom.ts
+++ src/store/purchaseAtom.ts
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist({
-  key: "sessionStorage", // 고유한 key 값
+  key: "sessionStorage",
   storage: sessionStorage,
 });
 
