43fb2d595aee15b1dca9193c42c68407ddc7112f
Yonghun <yonghuni484@gmail.com>
2023-11-23 23:29:45 +0900

    Feat: 상품 상세보기 api msw 연동

--- src/mocks/browser/handlers.ts
+++ src/mocks/browser/handlers.ts
@@ -1,6 +1,11 @@
 import { http, HttpResponse } from "msw";
 import { MockUpData } from "./constant";
-import { cartData, cartList, orderData } from "../../mock/myPageData";
+import {
+  cartData,
+  cartList,
+  orderData,
+  roomDetail,
+} from "../../mock/myPageData";
 import * as _ from "lodash";
 
 export const handlers = [
@@ -74,7 +79,8 @@ export const handlers = [
     });
   }),
 
-  // 주문 관련 post 요청
+  // 주문, 장바구니 관련 api 요청
+  // 상세 페이지에서 주문하기 눌러서 단일 상품 주문 요청
   http.post("/api/v1/rooms/:room_id/orders", async ({ request }) => {
     const newPost = (await request.json()) as {
       check_in_at: string;
@@ -99,6 +105,7 @@ export const handlers = [
     });
   }),
 
+  // 장바구니에서 상품 선택 후 주문 주문 요청
   http.post("/api/v1/baskets/orders", async () => {
     const data = [cartData[1], cartData[2]];
 
@@ -109,6 +116,7 @@ export const handlers = [
     });
   }),
 
+  // 상품 장바구니에 담기
   http.post("/api/v1/rooms/:room_id", async ({ request }) => {
     const newPost = (await request.json()) as {
       check_in_at: string;
@@ -132,6 +140,30 @@ export const handlers = [
     });
   }),
 
+  // 결제 생성 (결제하기 버튼 클릭할 때 요청)
+  http.post("/api/v1/payments/:order_id", async ({ request, params }) => {
+    const newPost = (await request.json()) as { payment_type: string };
+
+    const { order_id } = params;
+
+    const newData = orderData.filter((data) => data.order_id === ~~order_id)[0];
+
+    const resData = {
+      payment_id: 1,
+      payment_type: newPost.payment_type,
+      payment_at: new Date(),
+      total_price: 300100,
+      payment_status: "결제 완료",
+      order_datas: newData.order_datas,
+    };
+
+    return HttpResponse.json({
+      message: "성공",
+      data: resData,
+    });
+  }),
+
+  // 장바구니 조회
   http.get("/api/v1/baskets", async () => {
     return HttpResponse.json({
       message: "성공",
@@ -140,12 +172,59 @@ export const handlers = [
     });
   }),
 
+  // 주문 내역 조회 (아마 이후에 결제 완료 내역 조회로 수정 될 가능성 있음)
+  http.get("/api/v1/orders", async () => {
+    const newData = [...orderData[0].order_datas, ...orderData[1].order_datas];
+
+    return HttpResponse.json({
+      message: "성공",
+      order_datas: newData,
+    });
+  }),
+
+  // 주문 내역 상세 조회 (주문 번호로 조회할 수 있는 api)
   http.get("/api/v1/orders/:order_id", async ({ params }) => {
     const { order_id } = params;
+
     const newData = orderData.filter((data) => data.order_id === ~~order_id)[0];
+
     return HttpResponse.json({
       message: "성공",
       order_datas: newData.order_datas,
+    });
+  }),
+
+  // 장바구니 삭제 요청
+  http.delete("/api/v1/baskets", async ({ request }) => {
+    const delete_id = (await request.json()) as number;
+
+    return HttpResponse.json({
+      message: "성공",
+      delete_id: delete_id,
+    });
+  }),
+
+  // 숙소 상세정보에 관한 api요청
+  // 숙소 id로 해당 숙소 상세정보 보기
+  http.get("/api/v1/accommodations/:accommodation_id", async () => {
+    // const { accommodation_id } = params;
+    const resData = roomDetail;
+
+    return HttpResponse.json({
+      message: "성공",
+      data: resData,
+    });
+  }),
+
+  // 숙소에 포함된 방 id로 해당 방에 대한 정보 보기
+  http.get("/api/v1/rooms/:room_id", async ({ params }) => {
+    const { room_id } = params;
+
+    const resData = roomDetail.rooms.filter((room) => room.id === ~~room_id);
+
+    return HttpResponse.json({
+      message: "성공",
+      data: resData,
     });
   }),
 ];
