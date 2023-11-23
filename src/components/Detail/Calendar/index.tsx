98facb2cd8975ccebbb1064ec014beafe806cb06
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-23 11:30:38 +0900

    Feat: priceDisplay에 calculateNightCount 적용

--- src/components/Detail/Calendar/index.tsx
+++ src/components/Detail/Calendar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import * as Styled from "./Calendar.styles.ts";
@@ -14,24 +14,23 @@ const Calendar: React.FC<CalendarProps> = ({ price }) => {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
   const [endDate, setEndDate] = useState<Date | null>(null);
 
+  // 날짜를 로컬시간대로 변환하고, yyyy-mm-dd 형식으로 변환.
+  const formatDate = (date: Date | null) => {
+    if (!date) return "";
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
   // 날짜 변경 핸들러
   const handleChange = (dates: Date[]) => {
     const [start, end] = dates;
     setStartDate(start);
     setEndDate(end);
-    // setEndDate(end ? end : null);
 
     console.log("체크인 날짜:", start);
     console.log("체크아웃 날짜:", end);
-
-    // 날짜를 로컬시간대로 변환하고, yyyy-mm-dd 형식으로 변환.
-    const formatDate = (date: Date | null) => {
-      if (!date) return "";
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, "0");
-      const day = String(date.getDate()).padStart(2, "0");
-      return `${year}-${month}-${day}`;
-    };
 
     // 리코일 상태 업데이트
     setRoomDetail((currentState) => ({
@@ -43,6 +42,18 @@ const Calendar: React.FC<CalendarProps> = ({ price }) => {
       },
     }));
   };
+
+  // 컴포넌트 마운트 시 기본 체크인 및 체크아웃 날짜 설정
+  useEffect(() => {
+    setRoomDetail((currentState) => ({
+      ...currentState,
+      roomDetail: {
+        ...currentState.roomDetail,
+        check_in_at: formatDate(startDate),
+        check_out_at: formatDate(endDate),
+      },
+    }));
+  }, [setRoomDetail, startDate, endDate]);
 
   return (
     <>
--- src/components/Detail/PriceDisplay/index.tsx
+++ src/components/Detail/PriceDisplay/index.tsx
@@ -1,13 +1,46 @@
 import * as Styled from "./PriceDisplay.styles";
-import { PriceDisplayProps } from "./PriceDisplay.types";
 import formatNumber from "../../../utils/formatNumber";
-// import calculateNightCount from "../../../utils/calculateNightCount";
+import calculateNightCount from "../../../utils/calculateNightCount";
+import { useRecoilValue } from "recoil";
+import { roomDetailState } from "../../../store/roomDetailAtom";
 
-const PriceDisplay: React.FC<PriceDisplayProps> = ({ price }) => (
-  <Styled.PriceContainer>
-    <Styled.SumText>합계</Styled.SumText>
-    <Styled.PriceText>￦ {formatNumber(price)}</Styled.PriceText>
-  </Styled.PriceContainer>
-);
+const PriceDisplay = () => {
+  const roomDetail = useRecoilValue(roomDetailState);
+  console.log("전체 roomDetail 상태:", roomDetail);
+
+  if (!roomDetail?.data?.length) {
+    return <div>데이터가 없습니다.</div>;
+  }
+
+  const room = roomDetail.data[0];
+  console.log("선택된 방 정보:", room);
+
+  if (room.price == null) {
+    return <div>가격 정보가 없습니다.</div>;
+  }
+
+  let totalPrice;
+
+  if (
+    roomDetail.roomDetail &&
+    roomDetail.roomDetail.check_in_at &&
+    roomDetail.roomDetail.check_out_at
+  ) {
+    // 체크인 및 체크아웃 날짜가 있는 경우
+    const { check_in_at, check_out_at } = roomDetail.roomDetail;
+    const nightCount = calculateNightCount(check_in_at, check_out_at);
+    totalPrice = nightCount * room.price;
+  } else {
+    // 체크인 및 체크아웃 날짜가 없는 경우, 기본 가격 사용
+    totalPrice = room.price;
+  }
+
+  return (
+    <Styled.PriceContainer>
+      <Styled.SumText>합계</Styled.SumText>
+      <Styled.PriceText>￦ {formatNumber(totalPrice)}</Styled.PriceText>
+    </Styled.PriceContainer>
+  );
+};
 
 export default PriceDisplay;
