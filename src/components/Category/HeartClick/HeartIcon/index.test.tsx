40344e64886446b3107a2b3fc3d96c9148dc877e
MinSeobKim <icdmiracle927@gmail.com>
2023-11-24 22:21:05 +0900

    Feat : it에서 test로 이름 변경

--- src/components/Category/HeartClick/HeartIcon/index.test.tsx
+++ src/components/Category/HeartClick/HeartIcon/index.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import HeartIcon from "./index";
 
 describe("HeartIcon", () => {
-  it("버튼 클릭했을 때 이미 가지고 있는 값이 true일 경우", () => {
+  test("버튼 클릭했을 때 이미 가지고 있는 값이 true일 경우", () => {
     render(<HeartIcon clicked={true} />);
     const svgElement = document.querySelector("svg");
     expect(svgElement).toHaveAttribute("fill", "red");
