91e7f821435068a3dacc7980eb859bd5fb8f48a9
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-24 17:31:32 +0900

    Feat: 체크인, 체크아웃 날짜 리코일 상태 업데이트

--- src/components/Detail/Calendar/index.tsx
+++ src/components/Detail/Calendar/index.tsx
@@ -1,33 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import * as Styled from "./Calendar.styles.ts";
 import "./Calendar.css";
 import { ko } from "date-fns/locale";
 import { CalendarProps } from "./Calendar.types.ts";
+import formatNumber from "../../../utils/formatNumber";
+import { useRecoilState } from "recoil";
+import {
+  checkInDateState,
+  checkOutDateState,
+} from "../../../store/checkInCheckOutAtom.ts";
 
 const Calendar: React.FC<CalendarProps> = ({ price }) => {
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
-  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [checkInDate, setCheckInDate] = useRecoilState(checkInDateState);
+  const [checkOutDate, setCheckOutDate] = useRecoilState(checkOutDateState);
+
+  // 날짜를 로컬시간대로 변환하고, yyyy-mm-dd 형식으로 변환.
+  const formatDate = (date: Date | null) => {
+    if (!date) return "";
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
 
   // 날짜 변경 핸들러
-  const handleChange = (dates: Date[]) => {
+  const handleChange = (dates: [Date, Date]) => {
     const [start, end] = dates;
-    setStartDate(start);
-    setEndDate(end);
+    setCheckInDate(start);
+    setCheckOutDate(end);
+
+    console.log("체크인 날짜:", formatDate(start));
+    console.log("체크아웃 날짜:", formatDate(end));
   };
 
   return (
     <>
       <Styled.TextContainer>
         <Styled.SelectDatesText>날짜 선택</Styled.SelectDatesText>
-        <Styled.PriceText>1박 가격 ￦ {price}</Styled.PriceText>
+        <Styled.PriceText>1박 가격 ￦ {formatNumber(price)}</Styled.PriceText>
       </Styled.TextContainer>
       <DatePicker
-        selected={startDate}
+        selected={checkInDate}
         onChange={handleChange}
-        startDate={startDate}
-        endDate={endDate}
+        startDate={checkInDate}
+        endDate={checkOutDate}
         monthsShown={2}
         selectsRange
         inline
