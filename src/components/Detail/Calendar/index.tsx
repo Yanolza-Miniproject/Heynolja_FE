6b5004abe330f4ac9ed66489b3e4585c8f5b38ad
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-24 17:35:19 +0900

    Refactoring: Calendar에서 날짜형식 formatDate.ts로 분리

--- src/components/Detail/Calendar/index.tsx
+++ src/components/Detail/Calendar/index.tsx
@@ -11,19 +11,11 @@ import {
   checkInDateState,
   checkOutDateState,
 } from "../../../store/checkInCheckOutAtom.ts";
+import formatDate from "../../../utils/formatDate";
 
 const Calendar: React.FC<CalendarProps> = ({ price }) => {
   const [checkInDate, setCheckInDate] = useRecoilState(checkInDateState);
   const [checkOutDate, setCheckOutDate] = useRecoilState(checkOutDateState);
-
-  // 날짜를 로컬시간대로 변환하고, yyyy-mm-dd 형식으로 변환.
-  const formatDate = (date: Date | null) => {
-    if (!date) return "";
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
-  };
 
   // 날짜 변경 핸들러
   const handleChange = (dates: [Date, Date]) => {
