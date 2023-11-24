1a9516ffa5535d63220bec08f5b88224cb4b7fda
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-24 16:37:52 +0900

    Feat: detailPageData.ts 생성

---
+++ src/mock/detailPageData.ts
@@ -0,0 +1,10 @@
+export const roomDetail = {
+  accommodation_name: "그랜드 하얏트 제주",
+  room_name: "오션뷰 스위트룸",
+  price: 390000,
+  stock: 5,
+  room_image_url: [
+    "https://github.com/Yanolza-Miniproject/frontend/assets/92326949/2c0134f2-6ba3-434c-8dca-6d5831bf6e24",
+    "https://github.com/Yanolza-Miniproject/frontend/assets/92326949/fd904255-0d68-46df-a091-18d6efc6427f",
+  ],
+};
