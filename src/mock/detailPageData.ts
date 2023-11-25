d95acbcdc1e1c71b34f2c5cba1f3776c7bb3b895
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-26 00:13:11 +0900

    Feat: 객실 상세페이지 get 요청

--- src/mock/detailPageData.ts
+++ src/mock/detailPageData.ts
@@ -1,4 +1,5 @@
 export const roomDetail = {
+  id: 1,
   accommodation_name: "그랜드 하얏트 제주",
   room_name: "오션뷰 스위트룸",
   price: 390000,
@@ -8,3 +9,60 @@ export const roomDetail = {
     "https://github.com/Yanolza-Miniproject/frontend/assets/92326949/fd904255-0d68-46df-a091-18d6efc6427f",
   ],
 };
+
+// export const rooms = [
+//   {
+//     accommodation_name: "호텔이름은 이거",
+//     id: 1,
+//     name: "Room1",
+//     price: 100,
+//     capacity: 2,
+//     inventory: 10,
+//     categoryTv: true,
+//     categoryPc: false,
+//     categoryInternet: true,
+//     categoryRefrigerator: true,
+//     categoryBathingFacilities: false,
+//     categoryDryer: true,
+//     room_image_url: [
+//       "https://example.com/room1/image1.jpg",
+//       "https://example.com/room1/image2.jpg",
+//     ],
+//   },
+//   {
+//     id: 2,
+//     accommodation_name: "펜션이름 어쩌구",
+//     name: "Room2",
+//     price: 120,
+//     capacity: 2,
+//     inventory: 8,
+//     categoryTv: true,
+//     categoryPc: true,
+//     categoryInternet: true,
+//     categoryRefrigerator: false,
+//     categoryBathingFacilities: true,
+//     categoryDryer: false,
+//     room_image_url: [
+//       "https://example.com/room2/image1.jpg",
+//       "https://example.com/room2/image2.jpg",
+//     ],
+//   },
+//   {
+//     id: 10,
+//     accommodation_name: "제주도 게스트하우스",
+//     name: "Room10",
+//     price: 200,
+//     capacity: 4,
+//     inventory: 5,
+//     categoryTv: true,
+//     categoryPc: true,
+//     categoryInternet: true,
+//     categoryRefrigerator: true,
+//     categoryBathingFacilities: true,
+//     categoryDryer: true,
+//     room_image_url: [
+//       "https://example.com/room10/image1.jpg",
+//       "https://example.com/room10/image2.jpg",
+//     ],
+//   },
+// ];
--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -8,9 +8,13 @@ import Calendar from "../../components/Detail/Calendar";
 import StockStatusBanner from "../../components/Detail/StockStatusBaner";
 import { roomDetail } from "../../mock/detailPageData.ts";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useGetRoomDetail } from "../../hooks/useDetailFetch.ts";
 
 const Detail = () => {
-  const { accommodation_name, room_name, price, room_image_url } = roomDetail;
+  const { roomId } = useParams(); // URL에서 room_id 추출
+  const { data, isLoading, error } = useGetRoomDetail(parseInt(roomId, 10));
+  const { accommodation_name, room_name, price, room_image_url } = roomDetail; // 이부분 나중에 삭제
   const [selectedGuests, setSelectedGuests] = useState(1);
   const [selectedCheckInDate, setSelectedCheckInDate] = useState<Date | null>(
     null,
@@ -18,6 +22,20 @@ const Detail = () => {
   const [selectedCheckOutDate, setSelectedCheckOutDate] = useState<Date | null>(
     null,
   );
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>객실 정보가 없습니다.</div>;
+  }
+  console.log("roomId", roomId);
+  console.log("데이터:", data);
+  // const roomDetails = data.data;
 
   // Calendar 컴포넌트에서 선택한 날짜가 변경될 때 호출되는 콜백 함수
   const handleDateChange = (
@@ -32,10 +50,12 @@ const Detail = () => {
     <Styled.container>
       <Styled.Layout>
         <ProductGallery images={room_image_url} />
+        {/* <ProductGallery images={roomDetails.room_image_url} /> */}
         <Styled.DetailsContainer>
           <Styled.HorizontalContainer>
             <ProductDetails
               roomName={room_name}
+              // roomName={roomDetails.name}
               name={accommodation_name}
               price={price}
             />
