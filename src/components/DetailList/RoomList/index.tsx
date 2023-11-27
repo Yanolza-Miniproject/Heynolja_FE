ee5106616f948bb1ec5d56c1c16ece1358c9c15a
suyeonnnnnnn <suyeon9923@naver.com>
2023-11-28 04:22:29 +0900

    Feat: Refactoring: RoomList에서 isInventoryAvilable를 RoomList.utils.ts로 분리

--- src/components/DetailList/RoomList/index.tsx
+++ src/components/DetailList/RoomList/index.tsx
@@ -6,42 +6,16 @@ import {
 } from "../../../store/checkInCheckOutAtom";
 import RoomItem from "../RoomItem";
 import * as Styled from "./RoomList.styles";
-import { RoomType } from "./RoomList.types";
+import isInventoryAvailable from "./RoomList.utils";
 
 const RoomList = () => {
   const checkInDate = useRecoilValue(checkInDateState) || new Date();
   const checkOutDate = useRecoilValue(checkOutDateState) || new Date();
   const { rooms } = accommodationDetail;
 
-  console.log("체크인 날짜:", checkInDate);
-  console.log("체크아웃 날짜:", checkOutDate);
-
-  const isInventoryAvailable = (room: RoomType) => {
-    if (!checkInDate || !checkOutDate) return true;
-
-    const startDate = new Date(checkInDate);
-    const endDate = new Date(checkOutDate);
-
-    if (room.RoomInventory) {
-      for (const inventoryInfo of room.RoomInventory) {
-        const formattedDate = "20" + inventoryInfo.date;
-        const inventoryDate = new Date(formattedDate + "T00:00:00.000Z");
-
-        if (
-          inventoryDate >= startDate &&
-          inventoryDate < endDate &&
-          inventoryInfo.inventory <= 0
-        ) {
-          return false;
-        }
-      }
-    }
-    console.log(`방 ID: ${room.id}, 재고 상태:`, room.RoomInventory);
-    return true;
-  };
-  const filteredRooms = rooms.filter(isInventoryAvailable);
-
-  console.log("필터링된 방 목록:", filteredRooms);
+  const filteredRooms = rooms.filter((room) =>
+    isInventoryAvailable(room, checkInDate, checkOutDate),
+  );
 
   return (
     <Styled.RoomList>
