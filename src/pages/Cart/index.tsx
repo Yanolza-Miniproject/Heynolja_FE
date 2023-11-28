c1877934cdcbb179eb030908e767ce73b57c3583
Yonghun <yonghuni484@gmail.com>
2023-11-29 00:40:57 +0900

    Feat: 장바구니 페이지 로그인 안하면 로그인 페이지로 이동

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,49 +1,20 @@
-// import CartList from "../../components/Cart/CartList";
-// import { useGetMyCart } from "../../hooks/useCartFetch";
-import { authInstance } from "../../hooks/useAxios";
-// import useGeolocation from "../../hooks/useGeolocation";
+import CartList from "../../components/Cart/CartList";
+import { useGetMyCart } from "../../hooks/useCartFetch";
+import { useNavigate } from "react-router";
 
 const Cart = () => {
-  // const locaition = useGeolocation();
-  // const { data } = useGetMyCart(); // 카트 목록 데이터 요청
+  const navigate = useNavigate();
+  const { data, isLoading, error } = useGetMyCart(); // 카트 목록 데이터 요청
 
-  const fetch = async () => {
-    const accessToken = localStorage.getItem("access_token");
-    console.log(accessToken);
-    try {
-      const response = await authInstance.get("/baskets");
+  if (isLoading) {
+    return <div>로딩중</div>;
+  }
 
-      console.log(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  if (error && error?.message === "Request failed with status code 401") {
+    navigate("/signin");
+  }
 
-  // const refl = async () => {
-  //   const a = localStorage.getItem("access_token");
-  //   const r = localStorage.getItem("refresh_token");
-  //   try {
-  //     const response = await baseInstance.post("/refresh", null, {
-  //       headers: {
-  //         access_token: a,
-  //         refresh_token: r,
-  //       },
-  //     });
-
-  //     console.log(response.data);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // console.log(data);
-
-  // if (isLoading) {
-  //   return <div>로딩중</div>;
-  // }
-
-  // return data && <CartList data={data.data.data.rooms} />;
-  return <button onClick={fetch}>asd</button>;
+  return data && <CartList data={data.data.data.rooms} />;
 };
 
 export default Cart;
