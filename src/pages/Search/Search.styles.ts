eff49b507eb438fb7da3eb5576332fb14c7452f1
mysdpy30s <mysdpy30s@gmail.com>
2023-11-28 15:47:41 +0900

    Fix: em -> rem으로 수정

--- src/pages/Search/Search.styles.ts
+++ src/pages/Search/Search.styles.ts
@@ -7,14 +7,14 @@ export const Container = styled.div`
   align-items: center;
 
   max-width: 55rem;
-  padding: 2em;
+  padding: 2rem;
   margin: 0 auto;
 
   background-color: white;
 
   h2 {
-    margin-top: 1em;
-    margin-bottom: 1em;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
 
     font-size: 1.25rem;
     font-weight: 700;
@@ -27,13 +27,13 @@ export const SearchCard = styled.div`
   justify-content: flex-start;
   align-items: flex-start;
 
-  padding: 0.7em;
-  gap: 0.6em;
+  padding: 0.7rem;
+  gap: 0.6rem;
 
   width: 100%;
   max-width: 60rem;
   height: 30rem;
 
   border: 1px solid #e6e6e6;
-  border-radius: 1em;
+  border-radius: 1rem;
 `;
