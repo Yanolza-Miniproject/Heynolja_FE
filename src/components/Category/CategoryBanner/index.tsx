0c6abe1d4982e4792a2b73a6ec5d55a2d7fc4636
MinSeobKim <icdmiracle927@gmail.com>
2023-11-29 21:52:13 +0900

    Feat : test 버튼 삭제

--- src/components/Category/CategoryBanner/index.tsx
+++ src/components/Category/CategoryBanner/index.tsx
@@ -2,22 +2,9 @@ import { memo } from "react";
 import SearchButton from "../SearchButton";
 import * as Styled from "./CategoryBanner.styles";
 import { CategoryBannerProps } from "./CategoryBanner.types";
-import { authInstance } from "../../../hooks/useAxios";
-import { fetchToken } from "../../../api/Auth";
 
 const CategoryBanner = memo(
   ({ searchFn, firstText, secondText }: CategoryBannerProps) => {
-    const handleTest = async () => {
-      const results = await authInstance.get("wish");
-      console.log(results);
-    };
-
-    const handleRefreshTest = async () => {
-      const refreshToken = localStorage.getItem("refreshToken");
-      alert(refreshToken);
-      const results = await fetchToken(refreshToken!);
-      alert(results);
-    };
     return (
       <>
         <Styled.Banner>
@@ -38,12 +25,6 @@ const CategoryBanner = memo(
               <SearchButton />
             </Styled.CategoryBannerSearchMotion>
           </Styled.CategoryBannerTextMotion>
-          <button type="button" onClick={handleTest}>
-            안녕
-          </button>
-          <button type="button" onClick={handleRefreshTest}>
-            리프래쉬
-          </button>
         </Styled.Banner>
       </>
     );
