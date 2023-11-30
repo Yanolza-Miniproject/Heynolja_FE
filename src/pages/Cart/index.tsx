7c9bca753fb6232efe2a924a8dc8d7ca66b7b62c
Yonghun <yonghuni484@gmail.com>
2023-11-30 17:39:16 +0900

    Feat: 인증 경고창 추가

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,9 @@
-import { useNavigate } from "react-router";
 import CartList from "../../components/Cart/CartList";
 import { useGetMyCart } from "../../hooks/useCartFetch";
+import { useAuthAlert } from "../../hooks/useAlert";
 
 const Cart = () => {
-  const navigate = useNavigate();
+  const swal = useAuthAlert();
   const { data, isLoading, error } = useGetMyCart(); // 카트 목록 데이터 요청
 
   if (isLoading) {
@@ -11,7 +11,7 @@ const Cart = () => {
   }
 
   if (error && error?.message === "Request failed with status code 401") {
-    navigate("/signin");
+    swal();
   }
 
   return data && <CartList data={data.data.data.rooms} />;
