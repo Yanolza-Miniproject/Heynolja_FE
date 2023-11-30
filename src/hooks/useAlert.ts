ea100733a4790731e33c403886e44a9283d8e797
Yonghun <yonghuni484@gmail.com>
2023-11-30 18:27:06 +0900

    Fix: 인증 경고 공통 에러핸들링

--- src/hooks/useAlert.ts
+++ src/hooks/useAlert.ts
@@ -1,27 +1,22 @@
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
 
 export const useAuthAlert = () => {
-  const nvaigate = useNavigate();
-
-  const alert = () => {
-    Swal.fire({
-      title: "인증이 만료되었습니다.",
-      text: "다시 로그인하시겠습니까?",
-      padding: "50px",
-      confirmButtonColor: "#FF5100",
-      confirmButtonText: "로그인",
-      showDenyButton: true,
-      denyButtonText: "회원가입",
-      denyButtonColor: "#001650",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        nvaigate("/signin");
-      } else if (result.isDenied) {
-        nvaigate("/signup");
-      }
-    });
-  };
-
-  return alert;
+  Swal.fire({
+    title: "인증이 만료되었습니다.",
+    text: "다시 로그인하시겠습니까?",
+    padding: "50px",
+    confirmButtonColor: "#FF5100",
+    confirmButtonText: "로그인",
+    showDenyButton: true,
+    denyButtonText: "회원가입",
+    denyButtonColor: "#001650",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      location.href = "/signin";
+    } else if (result.isDenied) {
+      location.href = "/signup";
+    } else if (result.isDismissed) {
+      location.href = "/signin";
+    }
+  });
 };
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,18 +1,8 @@
 import CartList from "../../components/Cart/CartList";
 import { useGetMyCart } from "../../hooks/useCartFetch";
-import { useAuthAlert } from "../../hooks/useAlert";
 
 const Cart = () => {
-  const swal = useAuthAlert();
-  const { data, isLoading, error } = useGetMyCart(); // 카트 목록 데이터 요청
-
-  if (isLoading) {
-    return <div>로딩중</div>;
-  }
-
-  if (error && error?.message === "Request failed with status code 401") {
-    swal();
-  }
+  const { data } = useGetMyCart(); // 카트 목록 데이터 요청
 
   return data && <CartList data={data.data.data.rooms} />;
 };
