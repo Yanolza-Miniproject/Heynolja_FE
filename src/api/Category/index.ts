8b1f3642f61fc29e0f4402723c2239acc873cf8f
MinSeobKim <icdmiracle927@gmail.com>
2023-12-08 12:44:23 +0900

    Feat : Reducer과 readMed md 작성

--- src/api/Category/index.ts
+++ src/api/Category/index.ts
@@ -2,35 +2,45 @@ import { CategoryFilterParams } from "../../pages/Category/Category.types";
 import { baseInstance, authInstance } from "../../hooks/useAxios";
 import { isLoggedIn } from "../../utils/isLogined";
 
-const validParams = (param: string, value: number | boolean) => {
-  return value === false ? {} : { [param]: String(value) };
-};
+// const validParams = (param: string, value: number | boolean) => {
+//   return value === false ? {} : { [param]: String(value) };
+// };
 
 export const fetchCatgory = async (
   pageParam: number,
   props: CategoryFilterParams,
 ) => {
-  const params = {
-    ...validParams("region01", props.region01),
-    ...validParams("type", props.type),
-    ...validParams("categoryParking", props.categoryParking),
-    ...validParams("categoryCooking", props.categoryCooking),
-    ...validParams("categoryPickup", props.categoryPickup),
-  };
-
-  console.log(pageParam);
+  // const params = {
+  //   ...validParams("region01", props.region01),
+  //   ...validParams("type", props.type),
+  //   ...validParams("categoryParking", props.categoryParking),
+  //   ...validParams("categoryCooking", props.categoryCooking),
+  //   ...validParams("categoryPickup", props.categoryPickup),
+  // };
 
   type paramsType3 = Record<keyof CategoryFilterParams, string>;
 
-  const params2: Partial<paramsType3> = {};
+  // const params: Partial<paramsType3> = {};
 
-  for (const key in props) {
-    if (props[key as keyof CategoryFilterParams] !== false) {
-      params2[key as keyof paramsType3] = String(
-        props[key as keyof CategoryFilterParams],
-      );
-    }
-  }
+  // for (const key in props) {
+  //   if (props[key as keyof CategoryFilterParams] !== false) {
+  //     params[key as keyof paramsType3] = String(
+  //       props[key as keyof CategoryFilterParams],
+  //     );
+  //   }
+  // }
+
+  const initialParams: Partial<paramsType3> = {};
+
+  const params: Partial<paramsType3> = Object.entries(props).reduce(
+    (acc, [key, value]) => {
+      if (value !== false) {
+        acc[key as keyof paramsType3] = String(value);
+      }
+      return acc;
+    },
+    initialParams,
+  );
 
   const baseUrl = `accommodations?page=${pageParam}`;
 
