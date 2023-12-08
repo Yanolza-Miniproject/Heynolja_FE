41dcadd9e1296d70833caabdf341c3a3bc7bac8c
mysdpy30s <mysdpy30s@gmail.com>
2023-12-09 01:40:59 +0900

    Refactoring: App.tsx에 lazy loading 방식 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,61 @@
+import React, { ComponentType, Suspense, lazy } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Cart from "./pages/Cart";
-import Complete from "./pages/Complete";
-import Detail from "./pages/Detail";
-import Main from "./pages/Main";
-import MyPage from "./pages/MyPage";
-import MyOrder from "./pages/MyPage/MyOrder";
-import NotFound from "./pages/NotFound";
-import Payment from "./pages/Payment";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
 import Layout from "./components/Common/Layout";
-import Category from "./pages/Category";
 import { Global } from "@emotion/react";
 import { globalStyle } from "./components/Common/Common.styles";
-import Search from "./pages/Search";
-import SearchList from "./pages/SearchList";
-import MyWishs from "./pages/MyPage/MyWishs";
-import DetailList from "./pages/DetailList";
 import { Loader } from "./components/Common/Loader";
+const Cart = lazy(() => import("./pages/Cart"));
+const Complete = lazy(() => import("./pages/Complete"));
+const Detail = lazy(() => import("./pages/Detail"));
+const Main = lazy(() => import("./pages/Main"));
+const MyPage = lazy(() => import("./pages/MyPage"));
+const MyOrder = lazy(() => import("./pages/MyPage/MyOrder"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Payment = lazy(() => import("./pages/Payment"));
+const Signin = lazy(() => import("./pages/Signin"));
+const Signup = lazy(() => import("./pages/Signup"));
+const Category = lazy(() => import("./pages/Category"));
+const Search = lazy(() => import("./pages/Search"));
+const SearchList = lazy(() => import("./pages/SearchList"));
+const MyWishs = lazy(() => import("./pages/MyPage/MyWishs"));
+const DetailList = lazy(() => import("./pages/DetailList"));
+
+const createRoute = (path: string, component: ComponentType) => ({
+  path,
+  element: (
+    <Suspense fallback={<Loader />}>{React.createElement(component)}</Suspense>
+  ),
+});
+
+const routes = [
+  createRoute("/", Main),
+  createRoute("signin", Signin),
+  createRoute("signup", Signup),
+  createRoute("detail", Detail),
+  createRoute("detailList", DetailList),
+  createRoute("cart", Cart),
+  createRoute("payment", Payment),
+  createRoute("complete/:id", Complete),
+  createRoute("mypage", MyPage),
+  createRoute("category", Category),
+  createRoute("mypage/myorder", MyOrder),
+  createRoute("search", Search),
+  createRoute("mypage/mywish", MyWishs),
+  createRoute("results", SearchList),
+  createRoute("*", NotFound),
+];
 
 const router = createBrowserRouter([
   {
     path: "",
     element: <Layout />,
-    children: [
-      { path: "/", element: <Main /> },
-      { path: "signin", element: <Signin /> },
-      { path: "signup", element: <Signup /> },
-      { path: "detail", element: <Detail /> },
-      { path: "detailList", element: <DetailList /> },
-      { path: "cart", element: <Cart /> },
-      { path: "payment", element: <Payment /> },
-      { path: "complete/:id", element: <Complete /> },
-      { path: "mypage", element: <MyPage /> },
-      { path: "category", element: <Category /> },
-      { path: "mypage/myorder", element: <MyOrder /> },
-      { path: "search", element: <Search /> },
-      { path: "mypage/mywish", element: <MyWishs /> },
-      { path: "results", element: <SearchList /> },
-      { path: "*", element: <NotFound /> },
-    ],
+    children: routes,
   },
 ]);
 
 function App() {
   return (
     <>
-      <Loader />
       <Global styles={globalStyle} />
       <RouterProvider router={router} />
     </>
