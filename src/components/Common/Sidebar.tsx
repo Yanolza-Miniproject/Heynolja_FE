6819efdc5d78311df306feb17fb7380a793c8e91
mysdpy30s <mysdpy30s@gmail.com>
2023-12-09 21:41:13 +0900

    Refactoring: 사이드바 스크롤링 보완을 위해 PolyFill 추가 구현

--- src/components/Common/Sidebar.tsx
+++ src/components/Common/Sidebar.tsx
@@ -1,19 +1,19 @@
 import * as Styled from "../../components/Common/Common.styles";
 import ScrollToTopIcon from "../../assets/svg/totop-icon.svg";
+import { windowScrollToPolyfill } from "seamless-scroll-polyfill/src/scroll.polyfill";
 
 function Sidebar() {
+  windowScrollToPolyfill();
+
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <Styled.SidebarContainer>
       <Styled.SidebarWrapper>
         <Styled.NavButton onClick={scrollToTop}>
-          <img src={ScrollToTopIcon} onClick={scrollToTop} />
+          <img src={ScrollToTopIcon} alt="to-top" />
           TOP
         </Styled.NavButton>
       </Styled.SidebarWrapper>
