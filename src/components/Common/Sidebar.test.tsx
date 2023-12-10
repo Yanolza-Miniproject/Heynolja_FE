eec7a3910b1eb3e3910f6dc63acfe384bfd809ef
mysdpy30s <mysdpy30s@gmail.com>
2023-12-10 18:22:36 +0900

    Fix: 사이드바 테스트코드 수정

--- src/components/Common/Sidebar.test.tsx
+++ src/components/Common/Sidebar.test.tsx
@@ -1,9 +1,13 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import Sidebar from "./Sidebar";
+import { windowScrollToPolyfill } from "seamless-scroll-polyfill";
+
+jest.mock("seamless-scroll-polyfill");
 
 describe("사이드바 버튼 테스트", () => {
   beforeEach(() => {
     window.scrollTo = jest.fn();
+    windowScrollToPolyfill();
   });
 
   test("TOP 버튼 클릭시 화면 최상단으로 스크롤이 이동되어야 한다", () => {
@@ -16,5 +20,6 @@ describe("사이드바 버튼 테스트", () => {
       top: 0,
       behavior: "smooth",
     });
+    expect(windowScrollToPolyfill).toHaveBeenCalled();
   });
 });
