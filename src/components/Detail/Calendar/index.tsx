96c80a799f0c50d10f973be86b4585b932b7131b
suyeonnnnnnn <suyeon9923@naver.com>
2023-12-11 03:48:09 +0900

    Refactoring: Calendar에서 isNaN을 Number.isNaN으로 수정

--- src/components/Detail/Calendar/index.tsx
+++ src/components/Detail/Calendar/index.tsx
@@ -17,7 +17,7 @@ const Calendar: React.FC<CalendarProps> = ({ price, onDateChange }) => {
   const [checkOutDate, setCheckOutDate] = useRecoilState(checkOutDateState);
 
   const isValidDate = (date: Date | null) => {
-    return date instanceof Date && !isNaN(date.getTime());
+    return date instanceof Date && !Number.isNaN(date.getTime());
   };
 
   const validCheckInDate = isValidDate(checkInDate) ? checkInDate : undefined;
